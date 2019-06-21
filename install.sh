7baa0c55e0435ae01624db2b8cb5c82c34653e0b
Guanvi <luo_1912@outlook.com>
2019-06-21 09:21:09 +0800

    coc

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ mkdir ~/.vim/autoload
 mkdir ~/.vim/colors
 mkdir ~/.vim/plugged
 cp dracula.vim ~/.vim/colors/
+cp coc-settings.json ~/.vim/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+
+echo "Run PlugInstall by ex-mode in the vim"
+echo "then Run CocInstall coc-dictionary coc-tag coc-word coc-emoji coc-omni coc-syntax coc-gocode  coc-json  

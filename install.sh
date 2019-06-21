9072bfd9033c8c30b41cc4822ddeb30166f4f081
Guanvi <luo_1912@outlook.com>
2019-06-21 09:13:32 +0800

    SuperVim

--- install.sh
+++ install.sh
@@ -1,27 +1,10 @@
-#!/bin/bash
-echo 安装开始...
-echo "正在创建~/.vim/autoload..."
-mkdir -p ~/.vim/autoload
-if [ $? -eq 0 ]
-then 
-	echo .vim/autoload创建成功!
-fi
-echo 正在安装vim-plug至~/.vim/autoload...
-
-cp plug.vim ~/.vim/autoload 
-if [ $? -eq 0 ]
-then 
-	echo vim-plug安装成功！
-fi
-echo 正在创建颜色主题目录~/vim/colors...
+cd ~/
+[ -e .vim ]&&mv .vim .vim.back-$RANDOM
+mkdir ~/.vim
+mkdir ~/.vim/autoload
 mkdir ~/.vim/colors
-if [ $? -eq 0 ]
-then 
-	echo .vim/colors创建成功!
-fi
-cp Tomorrow-Night-Bright.vim molokai* ~/.vim/colors 
-echo 正在加载.vimrc配置文件...
-cp .vimrc ~/ 
-echo ".vimrc加载成功!"
-echo 安装完成!
-echo 请在vim命令模式下输入PlugInstall安装其余插件.
+mkdir ~/.vim/plugged
+cp dracula.vim ~/.vim/colors/
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
